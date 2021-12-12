04ee97c9c14de6ed9af1a341c283ce6d10484297
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-12 17:36:19 +0300

    Добавлено несколько тестов и программа, которая берет средние значения. Рассчитано, что для каждого значения числа процессов по 5 запусков

---
+++ MPI/average.py
@@ -0,0 +1,30 @@
+test_count = 5
+max_deg = 6
+
+file_list = [str(2 ** i) + '_' + str(j) + '.txt' for i in range(max_deg + 1) for j in range(1, 1 + test_count)]
+#file_list += ['160_' + str(i) + '.txt' for i in range(1, 6)]
+data = []
+for file in file_list:
+    with open('tests/' + file, "r") as f:
+        while text := f.readline():
+            if text.find('The output (if any) follows:') == -1:
+                continue
+            else:
+                f.readline()  # там пустая строка
+                for i in range(10):
+                    text = f.readline()
+                    struct = text.split()
+                    data.append([int(struct[2][:-1]), int(struct[5][:-1]), float(struct[8][:-1]), float(struct[10])])
+
+average_data = {}
+for struct in data:
+    if (struct[0], struct[1]) not in average_data:
+        average_data[(struct[0], struct[1])] = struct
+    else:
+        average_data[(struct[0], struct[1])][2] += struct[2]
+        average_data[(struct[0], struct[1])][3] += struct[3]
+
+for key in average_data.keys():
+    average_data[key][2] /= test_count
+    average_data[key][3] /= test_count
+    print(average_data[key], end=',\n')
