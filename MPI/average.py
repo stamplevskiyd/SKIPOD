bead1485b91168ba06e38792b3cce95162d39694
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-13 14:08:52 +0300

    Added new tests

--- MPI/average.py
+++ MPI/average.py
@@ -1,17 +1,17 @@
 test_count = 5
-max_deg = 6
+segment_count = 10
+processes = [1, 2, 4, 8, 16, 32, 64, 80]
 
-file_list = [str(2 ** i) + '_' + str(j) + '.txt' for i in range(max_deg + 1) for j in range(1, 1 + test_count)]
-#file_list += ['160_' + str(i) + '.txt' for i in range(1, 6)]
+file_list = [str(i) + '.txt' for i in range(1, test_count + 1)]
 data = []
 for file in file_list:
-    with open('tests/' + file, "r") as f:
+    with open('tests/united_tests/' + file, "r") as f:
         while text := f.readline():
             if text.find('The output (if any) follows:') == -1:
                 continue
             else:
                 f.readline()  # там пустая строка
-                for i in range(10):
+                for i in range(segment_count * len(processes)):
                     text = f.readline()
                     struct = text.split()
                     data.append([int(struct[2][:-1]), int(struct[5][:-1]), float(struct[8][:-1]), float(struct[10])])
